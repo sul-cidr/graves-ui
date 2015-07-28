e2388cb08cfe69ea0485a4ad21e86ec7fd86fc79
David McClure <davidwilliammcclure@gmail.com>
2015-07-28 15:17:37 -0700

    Basic map instantiation, text/map layout.

--- src/javascripts/views/map.js
+++ src/javascripts/views/map.js
@@ -2,6 +2,7 @@
 
 import $ from 'jquery';
 import Backbone from 'backbone';
+import L from 'leaflet';
 
 
 export default Backbone.View.extend({
@@ -14,7 +15,24 @@ export default Backbone.View.extend({
    * Start the map.
    */
   initialize: function() {
-    console.log('map');
+    this._initLeaflet();
+  },
+
+
+  /**
+   * Spin up the Leaflet instance.
+   */
+  _initLeaflet: function() {
+
+    this.map = L.map(this.el);
+
+    // Create an OSM tile layer.
+    var url = '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    L.tileLayer(url).addTo(this.map);
+
+    // TODO: Where to focus?
+    this.map.setView([30, 110], 5);
+
   },
 
 
