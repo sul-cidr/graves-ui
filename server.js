c53c12840cf2a7e44c444cf04319a772751829da
David McClure <davidwilliammcclure@gmail.com>
2015-07-30 09:48:03 -0700

    Wiring up the default view.

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ let app = express();
 // Assign the port.
 app.set('port', process.env.PORT || 3000);
 
+// Set view directory.
+app.set('views', './app/views');
+app.set('view engine', 'jade');
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 
