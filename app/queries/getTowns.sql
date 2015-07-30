c23c50806c288c8f5f9db68a4c9c17df75d47b94
David McClure <davidwilliammcclure@gmail.com>
2015-07-30 14:12:58 -0700

    Wires up the /api/towns endpoint.

---
+++ app/queries/getTowns.sql
@@ -0,0 +1,2 @@
+
+SELECT * FROM towns_2000 LIMIT 100;
--- server.js
+++ server.js
@@ -2,10 +2,23 @@
 
 import express from 'express';
 import configExpress from './app/config/express'
+import config from 'config';
+import pg from 'pg';
+import Promise from 'bluebird';
+import preql from 'preql';
 
 
+// Init server.
 let app = express();
 configExpress(app);
+
+
+// Load SQL queries.
+let queries = preql.makeQueries('./app/queries');
+
+
+// Promise-ify PG.
+let db = Promise.promisifyAll(pg);
 
 
 app.get('/', (req, res) => {
@@ -14,7 +27,17 @@ app.get('/', (req, res) => {
 
 
 app.get('/api/towns', (req, res) => {
-  res.send('towns');
+
+  db
+  .connectAsync(config.postgres)
+  .spread((client, close) => {
+
+    queries.getTowns(client.query.bind(client), (err, r) => {
+      res.send(r.rows);
+    });
+
+  });
+
 });
 
 
