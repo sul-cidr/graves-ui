49359340f28be29beca56cd518e8de5609610869
David McClure <davidwilliammcclure@gmail.com>
2015-07-31 10:01:37 -0700

    Adds a /towns API endpoint.

--- application.rb
+++ application.rb
@@ -11,6 +11,24 @@ configure do
   set :views, "#{File.dirname(__FILE__)}/views"
 end
 
-get "/" do
+get '/' do
   slim :index
+end
+
+get '/towns' do
+
+  content_type :json
+
+  DB[:towns_2000]
+    .select(
+      Sequel.lit('ST_AsText(geom)').as(:geom),
+      Sequel.as(:ename1, :province),
+      Sequel.as(:ename2, :prefecture),
+      Sequel.as(:ename3, :county),
+      Sequel.as(:ename, :town),
+    )
+    .limit(100)
+    .to_a
+    .to_json
+
 end
