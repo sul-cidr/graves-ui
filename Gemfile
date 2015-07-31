e23fc3d420dab711faa53a7d9be13d17b9d0844f
David McClure <davidwilliammcclure@gmail.com>
2015-07-31 09:36:39 -0700

    Adds a console script.

--- Gemfile
+++ Gemfile
@@ -2,11 +2,13 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'sinatra-contrib'
 gem 'rake'
 gem 'require_all'
 gem 'dotenv'
 gem 'json'
 gem 'slim'
+gem 'sequel'
 
 group :development do
   gem 'foreman'
--- environment.rb
+++ environment.rb
@@ -1,9 +1,16 @@
 
 require 'sinatra'
 require 'dotenv'
+require 'sequel'
 
 Dotenv.load
 
 configure do
-  # TODO: Connect to database.
+  DB = Sequel.connect(
+    adapter:  'postgres',
+    host:     ENV['PG_HOST'],
+    database: ENV['PG_DATABASE'],
+    user:     ENV['PG_USER'],
+    password: ENV['PG_PASSWORD'],
+  )
 end
