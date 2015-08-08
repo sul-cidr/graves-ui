c138e88669dd32b40d3ea642e560f6ea2021b2ec
David McClure <davidwilliammcclure@gmail.com>
2015-08-07 17:32:38 -0700

    Adding lon/lats to the .section markup.

--- public/src/javascripts/views/sections.js
+++ public/src/javascripts/views/sections.js
@@ -1,5 +1,6 @@
 
 
+import $ from 'jquery';
 import View from '../lib/view';
 
 
@@ -9,11 +10,27 @@ export default View.extend({
   el: '#text',
 
 
+  events: {
+    'click .section': 'onClick',
+  },
+
+
   /**
    * Select sections.
    */
   initialize: function() {
     this.sections = this.$('.section');
+  },
+
+
+  /**
+   * When a section is clicked.
+   *
+   * @param {Object} e
+   */
+  onClick: function(e) {
+    this.sections.removeClass('active');
+    $(e.currentTarget).addClass('active');
   },
 
 
