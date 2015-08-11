cde06c19fdd1f74fdba8ba8bf7799a1fbac38ba0
David McClure <davidwilliammcclure@gmail.com>
2015-08-11 09:35:50 -0700

    Stubbing in a MiniDoc class.

---
+++ public/src/javascripts/lib/minidoc/minidoc.js
@@ -0,0 +1,20 @@
+
+
+class MiniDoc {
+
+
+  /**
+   * Wrap the source/target selections, inject markup.
+   *
+   * @param {String} source - A CSS selector for the element that's being
+   * represented on the mini doc.
+   *
+   * @param {String} target - A CSS selector for the element that should
+   * contain the rendered mini doc.
+   */
+  constructor(source, target) {
+    // TODO
+  }
+
+
+}
