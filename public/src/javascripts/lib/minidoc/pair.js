18c6780cd7e3143bfdea6317dfdc2d9da22bdad1
David McClure <davidwilliammcclure@gmail.com>
2015-08-12 09:31:08 -0700

    Blocking in event bindings in the Pair class.

--- public/src/javascripts/lib/minidoc/pair.js
+++ public/src/javascripts/lib/minidoc/pair.js
@@ -10,8 +10,80 @@ export default class Pair {
    * @param {Object} target
    */
   constructor(source, target) {
+
     this.source = source;
     this.target = target;
+
+    this._bindEvents();
+
+  }
+
+
+  /**
+   * Bind cursor events on the source and target.
+   */
+  _bindEvents() {
+
+    this.source
+      .on('mouseenter', this.highlightSource.bind(this))
+      .on('mouseleave', this.unhighlight.bind(this));
+
+    this.target
+      .on('mouseenter', this.highlightTarget.bind(this))
+      .on('mouseleave', this.unhighlight.bind(this));
+
+  }
+
+
+  /**
+   * Apply a source highlight.
+   */
+  highlightSource() {
+
+    this.target.classed({
+      'highlight': true,
+      'highlight-source': true
+    });
+
+    this.source.addClass(
+      'highlight highlight-source'
+    );
+
+  }
+
+
+  /**
+   * Apply a target highlight.
+   */
+  highlightTarget() {
+
+    this.target.classed({
+      'highlight': true,
+      'highlight-target': true
+    });
+
+    this.source.addClass(
+      'highlight highlight-target'
+    );
+
+  }
+
+
+  /**
+   * Unhighlight both elements.
+   */
+  unhighlight() {
+
+    this.target.classed({
+      'highlight': false,
+      'highlight-source': true,
+      'highlight-target': true
+    });
+
+    this.source.addClass(
+      'highlight highlight-target highlight-target'
+    );
+
   }
 
 
